ec173d09826f5a03b47bc82bbdd585dc9ff47939
ravinduyas <ravindu.yasanka21@gmail.com>
2024-07-12 22:45:17 +0530

    installing packages

--- whatsapp/babel.config.js
+++ whatsapp/babel.config.js
@@ -1,6 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    Plugins: ['react-native-reanimated/plugin']
   };
 };
