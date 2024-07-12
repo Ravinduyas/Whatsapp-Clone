27b580c5b6f31bd0367f34e704bb2c85bbd33b9b
ravinduyas <ravindu.yasanka21@gmail.com>
2024-07-13 01:37:13 +0530

    created index tsx

--- whatsapp/app/_layout.tsx
+++ whatsapp/app/_layout.tsx
@@ -6,17 +6,12 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/components/useColorScheme';
-
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
+
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -49,11 +44,8 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="index" options={{headerShown: false}}/>
       </Stack>
-    </ThemeProvider>
   );
 }
---
+++ whatsapp/app/index.tsx
@@ -0,0 +1,9 @@
+import { View,Text } from "react-native";
+const Page = ()=> {
+    return (
+        <View>
+            <text>First Page</text>
+        </View>
+    );
+};
+export default Page
