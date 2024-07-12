ce612a57135b4c7c3134b1f40b9fe56a87703285
ravinduyas <ravindu.yasanka21@gmail.com>
2024-07-13 03:00:31 +0530

    header creating

--- whatsapp/App.js
+++ whatsapp/App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Header from './src/Header';
+import Navigation from './src/Navigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <StatusBar backgroundColor='#0e806a' barStyle='light-content' />
+        <Header />
+
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+          {/* Your content here */}
+        </View>
+
+      </View>
+    </NavigationContainer>
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
